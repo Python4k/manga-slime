0e664810af86adf9cb4b3986d1b7b5faf7b50dac
python4k <python4k@vk.com>
2025-02-21 04:05:41 +0300

    B feat: create eureka server as "server"
    Used as service discovery server

---
+++ server/src/test/java/com/python4k/mangaslime/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.python4k.mangaslime.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
